8e75e6792968910298c5e7d6277798a4299db6af
f_khusainov <farrukh_kh@Farrukhs-MacBook-Pro.local>
2024-06-15 08:52:17 +0500

    Implemented bean lifecycle methods using Spring interfaces

--- src/main/java/kh/farrukh/springcore/lifecycle/bean/LaptopMaker.java
+++ src/main/java/kh/farrukh/springcore/lifecycle/bean/LaptopMaker.java
@@ -2,10 +2,12 @@ package kh.farrukh.springcore.lifecycle.bean;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class LaptopMaker {
+public class LaptopMaker implements InitializingBean, DisposableBean {
 
   public LaptopMaker() {
     System.out.println("LaptopMaker constructor");
@@ -16,18 +18,28 @@ public class LaptopMaker {
   }
 
   @PostConstruct
-  public void init2() {
-    System.out.println("PostConstruct: LaptopMaker init 2");
+  public void postConstruct2() {
+    System.out.println("PostConstruct: LaptopMaker postConstruct2");
   }
 
   @PostConstruct
-  public void init1() {
-    System.out.println("PostConstruct: LaptopMaker init 1");
+  public void postConstruct1() {
+    System.out.println("PostConstruct: LaptopMaker postConstruct1");
   }
 
   @PreDestroy
+  public void preDestroy() {
+    System.out.println("PreDestroy: LaptopMaker preDestroy");
+  }
+
+  @Override
+  public void afterPropertiesSet() {
+    System.out.println("InitializingBean: LaptopMaker afterPropertiesSet");
+  }
+
+  @Override
   public void destroy() {
-    System.out.println("PreDestroy: LaptopMaker destroy");
+    System.out.println("DisposableBean: LaptopMaker destroy");
   }
 
 }
